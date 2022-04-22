496c556625a1349dd31690a01b0afff04926cca2
Digital-Haunt <chrisjhi@umich.edu>
2022-04-22 00:29:58 -0400

    ViewModel setup
    ViewModel is ready to be instantiated in each Fragment, which can get/set the LiveData String to the current cat breed (using an observer)

--- app/src/main/java/com/example/cis436_proj3/ui/main/MainViewModel.java
+++ app/src/main/java/com/example/cis436_proj3/ui/main/MainViewModel.java
@@ -1,7 +1,36 @@
 package com.example.cis436_proj3.ui.main;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.cis436_proj3.MainActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private List<String> catBreedsList = new ArrayList<String>();
+    private List<MainActivity.Cat> catObjects = new ArrayList<MainActivity.Cat>();
+    private MutableLiveData<String> catBreed;
+
+    public MutableLiveData<String> getResult(){
+        if (catBreed == null) {
+            catBreed = new MutableLiveData<String>();
+
+        }
+        return catBreed;
+
+    }
+
+    public void setBreeds (ArrayList<String> breeds){
+
+        for (int i = breeds.size(); i < 0 ; i--) {
+            catBreedsList.add(breeds.get(i));
+        }
+
+    }
+
+
+
 }
